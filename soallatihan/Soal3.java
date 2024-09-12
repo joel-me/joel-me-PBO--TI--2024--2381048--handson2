6c21d4de63e604b991a215855d9fc3bfa3aaaaec
joelalwansembiring <2381048@unai.edu>
2024-09-12 08:26:59 +0700

    Update Soal3.java

--- soallatihan/Soal3.java
+++ soallatihan/Soal3.java
@@ -4,23 +4,19 @@ import java.util.Scanner;
 
 public class Soal3 {
     public static void main(String[] args) {
-        // Membuat scanner untuk membaca input dari pengguna
         Scanner input = new Scanner(System.in);
-
-        // Meminta pengguna untuk memasukkan dua angka
+        
         System.out.print("Masukkan angka pertama: ");
         double angka1 = input.nextDouble();
 
         System.out.print("Masukkan angka kedua: ");
         double angka2 = input.nextDouble();
 
-        // Meminta pengguna untuk memilih operator
         System.out.print("Pilih operasi (+, -, *, /): ");
         char operator = input.next().charAt(0);
 
         double hasil = 0;
 
-        // Menghitung berdasarkan operator yang dipilih
         switch (operator) {
             case '+':
                 hasil = angka1 + angka2;
@@ -44,7 +40,7 @@ public class Soal3 {
                 return;
         }
 
-        // Menampilkan hasil perhitungan
+   
         System.out.println("Hasil: " + hasil);
     }
 }
